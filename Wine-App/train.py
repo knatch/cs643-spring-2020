bf05f95039ce19b5b3f63b5b5362c2b1269f9917
Natch Khongpasuk <natchynaat@gmail.com>
2020-05-07 01:45:06 -0400

    remove comments from train.py

--- Wine-App/train.py
+++ Wine-App/train.py
@@ -14,40 +14,22 @@ sc = SparkContext("local", conf=conf)
 sc.setLogLevel("FATAL")
 sqlContext = SQLContext(sc)
 
-# train = sqlContext.read.csv("hdfs:///data/TraingingDataset.csv", header = True)
-# train.show()
 # https://spark.apache.org/docs/latest/api/python/pyspark.sql.html?
 df = sqlContext.read.format('com.databricks.spark.csv').options(header='true', inferschema='true', sep=';').load(NAMENODE + '/data/TrainingDataset.csv')  
 validateDf = sqlContext.read.format('com.databricks.spark.csv').options(header='true', inferschema='true', sep=';').load(NAMENODE + '/data/ValidationDataset.csv')  
-# df = sqlContext.read.csv('hdfs://ip-172-31-25-55.ec2.internal/data/TrainingDataset.csv', header='true', inferSchema='true', sep=';')
 
-# df.show(5)
-# df.printSchema()
 # dropping quality column
 newDf = df.select(df.columns[:11])
 
-# newDf.printSchema()
-# va = VectorAssembler(inputCols=newDf.columns, outputCol='features')
-
-# output = va.transform(df)
-# output.show(5)
 outputRdd = df.rdd.map(lambda row: LabeledPoint(row[-1], Vectors.dense(row[:11])))
-# print(outputRdd.take(5))
-# transformedNewDf = newDf.rdd.map(lambda data: Vectors.dense([float(c) for c in data]))
-# rdd = df.rdd.map(lambda row: LabeledPoint(row[11], va))
-# newDf.printSchema()
-# df.select(df.columns[:5]).show(5)
 
-# model = RandomForest.trainClassifier(outputRdd,numClasses=10,categoricalFeaturesInfo={}, numTrees=5, maxBins=32, maxDepth=4, seed=42)
 model = RandomForest.trainClassifier(outputRdd,numClasses=10,categoricalFeaturesInfo={}, numTrees=60, maxBins=32, maxDepth=4, seed=42)
-print(model)
 # print(model.toDebugString())
 
 validationOutputRdd = validateDf.rdd.map(lambda row: LabeledPoint(row[-1], Vectors.dense(row[:11])))
 
 predictions = model.predict(validationOutputRdd.map(lambda x: x.features))
 labelsAndPredictions = validationOutputRdd.map(lambda lp: lp.label).zip(predictions)
-# testErr = labelsAndPredictions.filter(lambda lp: lp[0] != lp[1]).count() / float(validationOutputRdd.count())
 
 metrics = MulticlassMetrics(labelsAndPredictions)
 # Overall statistics
